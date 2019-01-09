77f985cd98eb8cac274330174bd380a6b173f155
HoangHuy <hoanghuy226@gmail.com>
2019-01-09 11:40:13 +0700

    init

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const params = require("./params");
 const express = require('express');
 const app = express();
 const port = 4000 || process.env.PORT;
-const job = require('./sync_job/blockchain_to_redis.js');
+const sync_job = require('./app/jobs/blockchain_to_redis.js');
+const job = new sync_job();
+const sync_job2 = require('./app/jobs/SyncChainToRedis.js');
+const job2 = new sync_job2();
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
@@ -14,7 +16,10 @@ app.use('/', express.static('public_static'));
 
 app.get('/start', (req, res) => {
   console.log("**start job**");
-  job.start(function (answer) {
+  // job.start(function (answer) {
+  //   res.send(answer);
+  // })
+  job2.init(function (answer) {
     res.send(answer);
   })
 });
